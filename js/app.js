39284df910b176638e2a4c929a4dddb6ae179987
Mikalai Bury <burymm@gmail.com>
2015-08-06 00:16:24 +0300

    add feature to save items to db and get item from db

--- js/app.js
+++ js/app.js
@@ -25,31 +25,26 @@ function ApplicationController($scope, $http) {
 
     var productsURL = 'http://127.0.0.1:3000/api/products';
     var defaultData = [{
-        "id": "1",
         "name": "Audi TT",
         "cost": "15000",
         description: "MK2"
     },
         {
-            "id": "2",
             "name": "Volkswagen New Beetle",
             "cost": "7000",
             description: "Classic car"
         },
         {
-            "id": "3",
             "name": "Audi RS88",
             "cost": "75000",
             description: "Bullet"
         },
         {
-            "id": "4",
             "name": "Dodge Challenger",
             "cost": "35000",
             description: "Muscle car"
         },
         {
-            "id": "5",
             "name": "Nissan GTR",
             "cost": "50000",
             description: "Fast car from Japan"
@@ -59,8 +54,16 @@ function ApplicationController($scope, $http) {
     function getProducts() {
         $http.get(productsURL).
             then(function(response) {
-                console.log("success get", response);
-                return response.data;
+                var products = response.data;
+                console.log("----Products----", products);
+                if (_.isUndefined(products) || _.isNull(products) || products.length == 0) {
+                    setProducts(defaultData);
+                }
+                $scope.listOfProducts = products;
+                $scope.listOfProductsHeaders = getHeaders($scope.listOfProducts[0]);
+                $scope.isPossibleAddComment = false;
+                $scope.isAddingItem = false;
+
             }, function(response) {
                 console.log(response);
                 return null;
@@ -71,22 +74,13 @@ function ApplicationController($scope, $http) {
         $http.post(productsURL, data).
             then(function(response) {
                console.log("Success post", response);
+                getProducts();
             }, function(response) {
                 console.log("Cant put data", response);
             });
     }
 
-
-    var products = getProducts();
-    console.log("----Products----", products);
-    if (_.isUndefined(products) || _.isNull(products) || products.length == 0) {
-        setProducts(defaultData);
-    }
-    $scope.listOfProducts = defaultData;
-
-    $scope.listOfProductsHeaders = getHeaders($scope.listOfProducts[0]);
-
-    $scope.isPossibleAddComment = false;
+    getProducts();
 
     $scope.showItem = function(item) {
         $scope.singleItem = item;
@@ -98,8 +92,6 @@ function ApplicationController($scope, $http) {
         $scope.comment = "";
     };
 
-
-    $scope.isAddingItem = false;
     $scope.startAddingItem = function() {
         $scope.isAddingItem = true;
     };
@@ -113,7 +105,9 @@ function ApplicationController($scope, $http) {
         $scope.isAddingItem = false;
         $scope.newItem.id = getNewItemId($scope.listOfProducts);
         $scope.listOfProducts.push($scope.newItem);
+        setProducts($scope.newItem);
         $scope.newItem = {};
     };
+
 
 }
